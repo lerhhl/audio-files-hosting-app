63f4d5a547460c498231a043bbab9e541ba46d7b
Hwee Lern <lerhhl@gmail.com>
2025-04-11 23:03:08 +0800

    redirect user to home if not admin

--- src/app/users-management/page.tsx
+++ src/app/users-management/page.tsx
@@ -3,12 +3,18 @@ import { getAllUsersAction } from "@/actions/user";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import UsersManagementPage from "@/components/UsersManagementPage";
 import { verifySession } from "@/lib/session";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
+import { DEFAULT_HOME_PATH } from "../constants";
 
 export default async function UsersManagementPageWrapper() {
   await redirectToLoginIfSessionNotFound();
-  const { isAuth } = await verifySession();
+  const { isAuth, isAdmin } = await verifySession();
   const users = getAllUsersAction();
+
+  if (!isAdmin) {
+    redirect(DEFAULT_HOME_PATH);
+  }
 
   return (
     <Suspense fallback={<LoadingSpinner />}>
